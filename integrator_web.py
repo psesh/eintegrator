1f124983eef8a1d4f7b56fa9437950607f76f730
Pranay Seshadri <p.seshadri@imperial.ac.uk>
2020-09-02 18:24:25 +0100

    2D capability

--- integrator_web.py
+++ integrator_web.py
@@ -6,6 +6,7 @@ import numpy as np
 from equadratures import Parameter, Basis, Poly
 from copy import deepcopy
 import io
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -19,7 +20,7 @@ plt.rcParams["font.family"] = "Avenir", "Arial", "Helvetica", "sans-serif"
 greyCol = 220/255
 
 # not elegant...
-fields = ['N', 'lower', 'upper', 'mean', 'variance', 'expression']
+fields = ['N', 'lower', 'upper', 'mean', 'variance', 'expression', 'univariate', 'bivariate']
 
 @app.route('/')
 def index(result=''):
@@ -40,7 +41,7 @@ def integrate():
         result = 'Invalid distribution!'
         flash(result)
         return redirect('/')
-    
+
     session['formdata'] = deepcopy(request.form)
 
     valid_N = [str(i) for i in range(1, MAX_N+1)]
@@ -52,7 +53,7 @@ def integrate():
         return redirect('/')
     else:
         N = int(N)
-    
+
     lower = request.form['lower']
 
     if lower != '':
@@ -62,7 +63,7 @@ def integrate():
             result = 'Invalid lower bound!'
             flash(result)
             return redirect('/')
-    
+
     upper = request.form['upper']
     if upper != '':
         try:
@@ -71,7 +72,7 @@ def integrate():
             result = 'Invalid upper bound!'
             flash(result)
             return redirect('/')
-    
+
     mean = request.form['mean']
     if mean != '':
         try:
@@ -95,7 +96,7 @@ def integrate():
             return redirect('/')
 
     # TODO: alpha, beta etc.
-
+    dimensions = request.form['dimension_params']
     expr = request.form['expression']
 
     if distr == 'gaussian':
@@ -105,34 +106,47 @@ def integrate():
         A = None
         B = None
 
-    #TODO: handle invalid distribution, check type etc.
 
-    def f(x):
-        return ne.evaluate(expr)
+    if dimensions == 'bivariate':
+        def f(x):
+            x1 = x[0]
+            x2 = x[1]
+            return ne.evaluate(expr)
+    elif dimensions == 'univariate':
+        def f(x):
+            return ne.evaluate(expr)
+    else:
+        print('error!')
 
-    answer = calc_integral(f, N, distr, lower, upper, A, B)
+    answer = calc_integral(dimensions, f, N, distr, lower, upper, A, B)
     if answer == 'error':
         result = 'An error occurred during the polynomial quadrature process!'
         flash(result)
-    #else:
-    #    result = 'The integral is %f.' % answer
+
     return redirect('/')
 
-def calc_integral(f, N, distr, lower=None, upper=None, A=None, B=None):
+def calc_integral(case, f, N, distr, lower=None, upper=None, A=None, B=None):
     if N < 2:
         return 1.0
-    
+
     order = N - 1
 
     try:
-        my_param = Parameter(order, distr, lower=lower, upper=upper, shape_parameter_A=A, shape_parameter_B=B)
-        my_basis = Basis('univariate') # multivariate later?
-        my_poly = Poly(my_param, my_basis, method='numerical-integration')
+        # Univariate setup!
+        if case == 'univariate':
+            my_param = Parameter(order, distr, lower=lower, upper=upper, shape_parameter_A=A, shape_parameter_B=B)
+            my_basis = Basis('univariate')
+            my_poly = Poly(my_param, my_basis, method='numerical-integration')
+        elif case == 'bivariate':
+            my_param_1 = Parameter(order, distr, lower=lower, upper=upper, shape_parameter_A=A, shape_parameter_B=B)
+            my_param_2 = Parameter(order, distr, lower=lower, upper=upper, shape_parameter_A=A, shape_parameter_B=B)
+            my_basis = Basis('tensor-grid')
+            my_poly = Poly([my_param_1, my_param_2], my_basis, method='numerical-integration')
         my_poly.set_model(f)
+
     except:
         return 'error'
-    
-    print('ok')
+
     if distr == 'uniform':
         interval = upper - lower
         plot_lower = lower - 1.0 * interval
@@ -146,18 +160,37 @@ def calc_integral(f, N, distr, lower=None, upper=None, A=None, B=None):
         plot_upper = mean + 3.0 * stddev
         session['x_lower'] = plot_lower
         session['x_upper'] = plot_upper
-    
-    session['plot_lower'] = plot_lower
-    session['plot_upper'] = plot_upper
-    plot_test = np.linspace(plot_lower, plot_upper, 50)
-    session['pdf_vals'] = pickle.dumps(my_param.get_pdf(plot_test), protocol=0)
-    session['fit_vals'] = pickle.dumps(my_poly.get_polyfit(plot_test).reshape(-1), protocol=0)
-    session['quad_points'] = pickle.dumps(my_poly._quadrature_points, protocol=0)
-    session['quad_evals'] = pickle.dumps(my_poly.get_polyfit(my_poly._quadrature_points).reshape(-1), protocol=0)
-    session['integral_value'] = float(my_poly.coefficients[0])
 
-    return float(my_poly.coefficients[0])
+    if case == 'univariate':
+        plot_test = np.linspace(session['x_lower'], session['x_upper'], 50)
+        session['pdf_vals'] = pickle.dumps(my_param.get_pdf(plot_test), protocol=0)
+        session['fit_vals'] = pickle.dumps(my_poly.get_polyfit(plot_test).reshape(-1), protocol=0)
+        session['quad_points'] = pickle.dumps(my_poly._quadrature_points, protocol=0)
+        session['quad_evals'] = pickle.dumps(my_poly.get_polyfit(my_poly._quadrature_points).reshape(-1), protocol=0)
+        session['integral_value'] = float(my_poly.coefficients[0])
+        flash(session['integral_value'])
 
+    elif case == 'bivariate':
+        K = 10
+        N = K*K
+        xi = np.linspace(float(session['x_lower']), float(session['x_upper']), K)
+        yi = np.linspace(float(session['x_lower']), float(session['x_upper']), K)
+        xx, yy = np.meshgrid(xi, yi)
+        xxi = xx.reshape(N, 1)
+        yyi = yy.reshape(N, 1)
+        Xout = np.hstack([xxi, yyi])
+        zz = my_poly.get_polyfit(Xout).reshape(K, K)
+        session['poly_surface'] =  pickle.dumps(zz, protocol=0)
+        session['poly_surface_xx'] =  pickle.dumps(xx, protocol=0)
+        session['poly_surface_yy'] =  pickle.dumps(yy, protocol=0)
+        session['quad_points'] = pickle.dumps(my_poly._quadrature_points, protocol=0)
+        session['quad_evals'] = pickle.dumps(my_poly.get_polyfit(my_poly._quadrature_points).reshape(-1), protocol=0)
+        session['integral_value'] = float(my_poly.coefficients[0])
+        flash(session['integral_value'])
+
+    session['dimensions'] = case
+    print('got here!')
+    return session['integral_value']
 @app.route('/plot.png')
 def plot_png():
     fig = create_figure()
@@ -173,58 +206,66 @@ def plot_png_d():
     return Response(output.getvalue(), mimetype='image/png')
 
 def create_figure(detailed=False):
+    print('Creating figure!')
     if not('formdata' in session):
         fig = Figure()
         fig.patch.set_facecolor( (greyCol, greyCol, greyCol, 0.5) )
         axis = fig.add_subplot(1, 1, 1, facecolor="none")
         return fig
-
     try:
+        print('Line 215')
         params = session['formdata']
-        #print(params)
         fig = Figure()
         fig.patch.set_facecolor( (greyCol, greyCol, greyCol, 0.5) )
         axis = fig.add_subplot(1, 1, 1, facecolor="none")
+        print('Line 222')
+        if session['dimensions'] == 'univariate':
+            plot_test = np.linspace(float(session['x_lower']), float(session['x_upper']), 50)
+            axis.set_xlim([   float(session['x_lower']), float(session['x_upper'])   ])
+            expr = params['expression']
+            def f(x):
+                return ne.evaluate(expr)
+            f_val = f(plot_test)
+            fx_ln = axis.plot(plot_test, f_val, label='f(x)')
+            legends = ['f(x)', 'Area', 'Quadrature points']
+            pdf_vals = pickle.loads(session['pdf_vals'])
+            fit_vals = pickle.loads(session['fit_vals'])
+            ymin = np.min( np.r_[f_val, pdf_vals, fit_vals] )
+            ymax = np.max( np.r_[f_val, pdf_vals, fit_vals] )
+            axis.set_ylim([1.1*ymin, 1.1*ymax])
+            if detailed:
+                axis2 = axis.twinx()
+                #axis2.set_yticks([])
+                pdf_ln = axis2.plot(plot_test, pdf_vals, 'g--', alpha=0.5, label='PDF (scaled)')
+                fit_ln = axis.plot(plot_test, fit_vals, 'r--', alpha=0.5, label='Poly fit')
+            quad_pts = pickle.loads(session['quad_points'])
+            quad_evals = pickle.loads(session['quad_evals'])
+            integral_val = session['integral_value']
+            x_test = np.linspace(float(session['x_lower']), float(session['x_upper']), 20)
+            int_fill = axis.fill_between(x_test, 0.0*x_test, f(x_test), alpha=0.5, hatch='/', label='Area')
+            axis.set_title('Integral value: %f' % integral_val)
+            quad_sc = axis.scatter(quad_pts, quad_evals, label='Quadrature points')
+            if detailed:
+                lns = [fx_ln[0], pdf_ln[0], fit_ln[0], int_fill, quad_sc]
+            else:
+                lns = [fx_ln[0], int_fill, quad_sc]
+            legends = [l.get_label() for l in lns]
+            axis.legend(lns, legends, framealpha=0.0)
+        elif session['dimensions'] == 'bivariate':
+            print('reached here!!!!!')
+            quad_pts = pickle.loads(session['quad_points'])
+            quad_evals = pickle.loads(session['quad_evals'])
+            xx = pickle.loads(session['poly_surface_xx'])
+            yy = pickle.loads(session['poly_surface_yy'])
+            zz = pickle.loads(session['poly_surface'])
+            integral_val = session['integral_value']
 
-        plot_lower = session['plot_lower']
-        plot_upper = session['plot_upper']
-
-        plot_test = np.linspace(plot_lower, plot_upper, 50)
-        axis.set_xlim([plot_lower, plot_upper])
-        expr = params['expression']
-        def f(x):
-            return ne.evaluate(expr)
-
-        f_val = f(plot_test)
-        fx_ln = axis.plot(plot_test, f_val, label='f(x)')
-        legends = ['f(x)', 'Area', 'Quadrature points']
-        pdf_vals = pickle.loads(session['pdf_vals'])
-        fit_vals = pickle.loads(session['fit_vals'])
-        ymin = np.min( np.r_[f_val, pdf_vals, fit_vals] )
-        ymax = np.max( np.r_[f_val, pdf_vals, fit_vals] )
-        axis.set_ylim([1.1*ymin, 1.1*ymax])
-        if detailed:
-            axis2 = axis.twinx()
-            #axis2.set_yticks([])
-            pdf_ln = axis2.plot(plot_test, pdf_vals, 'g--', alpha=0.5, label='PDF (scaled)')
-            fit_ln = axis.plot(plot_test, fit_vals, 'r--', alpha=0.5, label='Poly fit')
-
-
-        quad_pts = pickle.loads(session['quad_points'])
-        quad_evals = pickle.loads(session['quad_evals'])
-        integral_val = session['integral_value']
-
-        x_test = np.linspace(float(session['x_lower']), float(session['x_upper']), 20)
-        int_fill = axis.fill_between(x_test, 0.0*x_test, f(x_test), alpha=0.5, hatch='/', label='Area')
-        axis.set_title('Integral value: %f' % integral_val)
-        quad_sc = axis.scatter(quad_pts, quad_evals, label='Quadrature points')
-        if detailed:
-            lns = [fx_ln[0], pdf_ln[0], fit_ln[0], int_fill, quad_sc]
-        else:
-            lns = [fx_ln[0], int_fill, quad_sc]
-        
-        legends = [l.get_label() for l in lns]
-        axis.legend(lns, legends, framealpha=0.0)
+            norm = matplotlib.colors.Normalize(vmin=np.min(zz),vmax=np.max(zz))
+            axis.contourf(xx, yy, zz, levels=20, norm=norm)
+            axis.set_title('Polynomial response surface and quadrature points. \n Integral value: '+str(np.round(integral_val, 4)))
+            axis.scatter(quad_pts[:,0], quad_pts[:,1], c=quad_evals, s=100, zorder=2, edgecolor='black', norm=norm, label='Quadrature points')
+            #val = axis.set_xlabel('x1')
+            #val2 = axis.set_ylabel('x2')
     except:
         fig = Figure()
         fig.patch.set_facecolor( (greyCol, greyCol, greyCol, 0.5) )
@@ -232,6 +273,7 @@ def create_figure(detailed=False):
         return fig
 
     return fig
+
 
 @app.route('/clear', methods = ['GET','POST'])
 def clear():
